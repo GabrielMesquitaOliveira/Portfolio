7a422836b9f2b562b5da6dca8465e0122fff2a00
Gabriel <92613742+GabrielMesquitaOliveira@users.noreply.github.com>
2025-09-10 11:21:57 -0300

    feat: tape Section

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,42 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Perfomant",
+  "Accessible",
+  "Responsive",
+  "Intuitive",
+  "Dynamic",
+  "Engaging",
+  "Secuere",
+  "Scalable",
+  "Optimized",
+  "Innovative",
+  "Robust",
+  "Maintainable",
+  "Cutting-Edge",
+  "User-Centric",
+  "Seamless",
+  "Interactive",
+  "Fast-Loading",
+]
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 overflow-x-clip">
+      <div className="bg-gradient-to-r from-violet-300 to-sky-400 -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word, index) => (
+              <div key={index} className="inline-flex gap-4 items-center">
+                <span className="font-extrabold text-sm text-gray-900 ">
+                  {word}
+                </span>
+                <StarIcon className="size-6 text-gray-900" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
