22dd1bfaf0e1fb52177da4d740da0ed375520ea5
Gabriel <92613742+GabrielMesquitaOliveira@users.noreply.github.com>
2025-09-10 23:15:38 -0300

    `Added marquee effect to TapeSection and updated global styles and theme variables`

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,13 +1,14 @@
+"use client";
 import StarIcon from "@/assets/icons/star.svg";
 
 const words = [
-  "Perfomant",
+  "Performant",
   "Accessible",
   "Responsive",
   "Intuitive",
   "Dynamic",
   "Engaging",
-  "Secuere",
+  "Secure",
   "Scalable",
   "Optimized",
   "Innovative",
@@ -18,22 +19,34 @@ const words = [
   "Seamless",
   "Interactive",
   "Fast-Loading",
-]
+];
 
 export const TapeSection = () => {
   return (
     <div className="py-16 overflow-x-clip">
       <div className="bg-gradient-to-r from-violet-300 to-sky-400 -rotate-3 -mx-1">
-        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word, index) => (
-              <div key={index} className="inline-flex gap-4 items-center">
-                <span className="font-extrabold text-sm text-gray-900 ">
-                  {word}
-                </span>
-                <StarIcon className="size-6 text-gray-900" />
-              </div>
-            ))}
+        <div className="[mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <div className="marquee-container">
+            <div className="marquee-content">
+              {words.map((word, index) => (
+                <div key={index} className="marquee-item">
+                  <span className="font-extrabold text-sm text-gray-900">
+                    {word}
+                  </span>
+                  <StarIcon className="size-6 text-gray-900" />
+                </div>
+              ))}
+            </div>
+            <div className="marquee-content py-2" aria-hidden="true">
+              {words.map((word, index) => (
+                <div key={`duplicate-${index}`} className="marquee-item">
+                  <span className="font-extrabold text-sm text-gray-900">
+                    {word}
+                  </span>
+                  <StarIcon className="size-6 text-gray-900" />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
